e3be95a6755eed784270487f1f091ed3c28e5814
sanjeewa999 <sanjeewachaturanga0@gmail.com>
2023-04-14 21:58:32 +0530

    UserController added

---
+++ Backend/Employee Registration/Employee-Registration/src/main/java/com/CRUD/Employee/Registration/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.CRUD.Employee.Registration.controller;
+
+import com.CRUD.Employee.Registration.model.User;
+import com.CRUD.Employee.Registration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+}
